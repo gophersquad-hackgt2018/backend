168fd9ce15bfe6580c7cbad9e46bce29edaa14ca
Daniel Hsing <hsing.daniel@gmail.com>
2018-10-19 23:38:32 -0400

    feat: add endpoint for handling image uploads
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

--- api.js
+++ api.js
@@ -1,9 +1,19 @@
 const router = require("express").Router();
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 router.get("/", (req, res) => {
     res.json({
         success: true,
         message: "Server is healthy"
+    });
+});
+
+router.post("/upload", upload.single("image"), (req, res) => {
+    console.log(`Image received with ${req.file.size} bytes`);
+    res.json({
+        success: true,
+        message: "Image upload was successful"
     });
 });
 

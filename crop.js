4cea205c85460445f7bdbaa58c7cf4c4f29a2b85
Daniel Hsing <hsing.daniel@gmail.com>
2018-10-20 16:26:26 -0400

    chore: reduce log clutter
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

--- crop.js
+++ crop.js
@@ -1,85 +1,92 @@
-const cv = require('opencv4nodejs')
+const cv = require("opencv4nodejs");
 
 // Read image and search for contours
 // const img = cv.imread('./pic2.jpg')
 // const img2 = cv.imread('./pic2.jpg')
 
 function getCrops(filename) {
-  const img = cv.imread(`./uploads/${filename}`)
-  const gray = img.cvtColor(cv.COLOR_BGR2GRAY)
-  let threshold = gray.adaptiveThreshold(255, cv.ADAPTIVE_THRESH_GAUSSIAN_C,
-      cv.THRESH_BINARY, 31, 20)
-  // cv.imshow('a window name', gray.resize(0, 0, 0.9, 0.9))
-  // cv.waitKey()
-  // cv.imshow('a window name', threshold.resize(0, 0, 0.9, 0.9))
-  // cv.waitKey()
-  let contours = threshold.findContours(cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
-
-  // Create first mask for rotation
-  let mask = new cv.Mat(img.rows, img.cols, cv.CV_8U, 255)
-
-  // Draw contours on the mask with size and ratio of borders for threshold
-  contours.forEach((cnt) => {
-    let size = cnt.area
-    let x = cnt.boundingRect().x
-    let y = cnt.boundingRect().y
-    let w = cnt.boundingRect().width
-    let h = cnt.boundingRect().height
-    if (size > 10000) {
-      console.log(size)
-    }
-    if (10000 > size && size > 35 && w * 2.5 > h) {
-      // console.log(cnt)
-      mask.drawContours([cnt], new cv.Vec3(0, 0, 0), -1, cv.LINE_8, -1)
-    }
-  })
-
-  // Connect neighbour contours and select the biggest one (text).
-  let kernel = new cv.Mat(25, 80, cv.CV_8U, 1)
-  let gray_op = mask.morphologyEx(kernel, cv.MORPH_OPEN)
-  let threshold_op = gray_op.threshold(170, 255, cv.THRESH_BINARY_INV)
-
-  // Remove holes
-  let floodfill = threshold_op.copy()
-  let floodMask = new cv.Mat(threshold_op.sizes[0], threshold_op.sizes[1],
-      cv.CV_8U, 0)
-  floodfill.floodFill(new cv.Point2(0, 0), 255)
-  let floodfillInv = floodfill.bitwiseNot()
-  let out = threshold_op.bitwiseOr(floodfillInv)
-  let contours_op = out.findContours(cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
-
-  // cv.imshow('a window name', mask.resize(0, 0, 0.9, 0.9))
-  // cv.waitKey()
-  // cv.imshow('a window name', gray_op.resize(0, 0, 0.9, 0.9))
-  // cv.waitKey()
-  // cv.imshow('a window name', threshold_op.resize(0, 0, 0.9, 0.9))
-  // cv.waitKey()
-  // cv.imshow('a window name', out.resize(0, 0, 0.9, 0.9))
-  // cv.waitKey()
-
-  // contours_op.sort((a, b) => {
-  //   return a.area - b.area
-  // })
-
-  let names = []
-  contours_op.forEach((cont, ind) => {
-    // gray.drawRectangle(cont.boundingRect(), new cv.Vec3(0,0,255), 3)
-    // cv.imshow('a window name', threshold.getRegion(cont.boundingRect()))
+    const img = cv.imread(`./uploads/${filename}`);
+    const gray = img.cvtColor(cv.COLOR_BGR2GRAY);
+    let threshold = gray.adaptiveThreshold(
+        255,
+        cv.ADAPTIVE_THRESH_GAUSSIAN_C,
+        cv.THRESH_BINARY,
+        31,
+        20
+    );
+    // cv.imshow('a window name', gray.resize(0, 0, 0.9, 0.9))
     // cv.waitKey()
-    cv.imwrite(`./crops/${filename}-${ind}.jpg`, threshold.getRegion(cont.boundingRect()))
-    // out.push(gray.getRegion(cont.boundingRect()).getData())
-    names.push(`${filename}-${ind}.jpg`)
-    //  Replace region with white so we don't do things twice
-    gray.drawRectangle(cont.boundingRect(), new cv.Vec3(255, 255, 255), -1)
-  })
-  // cv.imshow('a window name', gray.resize(0, 0, 0.9, 0.9))
-  // cv.waitKey()
-  console.log(names)
-  return names
+    // cv.imshow('a window name', threshold.resize(0, 0, 0.9, 0.9))
+    // cv.waitKey()
+    let contours = threshold.findContours(cv.RETR_TREE, cv.CHAIN_APPROX_NONE);
+
+    // Create first mask for rotation
+    let mask = new cv.Mat(img.rows, img.cols, cv.CV_8U, 255);
+
+    // Draw contours on the mask with size and ratio of borders for threshold
+    contours.forEach(cnt => {
+        let size = cnt.area;
+        let x = cnt.boundingRect().x;
+        let y = cnt.boundingRect().y;
+        let w = cnt.boundingRect().width;
+        let h = cnt.boundingRect().height;
+        if (10000 > size && size > 35 && w * 2.5 > h) {
+            mask.drawContours([cnt], new cv.Vec3(0, 0, 0), -1, cv.LINE_8, -1);
+        }
+    });
+
+    // Connect neighbour contours and select the biggest one (text).
+    let kernel = new cv.Mat(25, 80, cv.CV_8U, 1);
+    let gray_op = mask.morphologyEx(kernel, cv.MORPH_OPEN);
+    let threshold_op = gray_op.threshold(170, 255, cv.THRESH_BINARY_INV);
+
+    // Remove holes
+    let floodfill = threshold_op.copy();
+    let floodMask = new cv.Mat(
+        threshold_op.sizes[0],
+        threshold_op.sizes[1],
+        cv.CV_8U,
+        0
+    );
+    floodfill.floodFill(new cv.Point2(0, 0), 255);
+    let floodfillInv = floodfill.bitwiseNot();
+    let out = threshold_op.bitwiseOr(floodfillInv);
+    let contours_op = out.findContours(cv.RETR_TREE, cv.CHAIN_APPROX_NONE);
+
+    // cv.imshow('a window name', mask.resize(0, 0, 0.9, 0.9))
+    // cv.waitKey()
+    // cv.imshow('a window name', gray_op.resize(0, 0, 0.9, 0.9))
+    // cv.waitKey()
+    // cv.imshow('a window name', threshold_op.resize(0, 0, 0.9, 0.9))
+    // cv.waitKey()
+    // cv.imshow('a window name', out.resize(0, 0, 0.9, 0.9))
+    // cv.waitKey()
+
+    // contours_op.sort((a, b) => {
+    //   return a.area - b.area
+    // })
+
+    let names = [];
+    contours_op.forEach((cont, ind) => {
+        // gray.drawRectangle(cont.boundingRect(), new cv.Vec3(0,0,255), 3)
+        // cv.imshow('a window name', threshold.getRegion(cont.boundingRect()))
+        // cv.waitKey()
+        cv.imwrite(
+            `./crops/${filename}-${ind}.jpg`,
+            threshold.getRegion(cont.boundingRect())
+        );
+        // out.push(gray.getRegion(cont.boundingRect()).getData())
+        names.push(`${filename}-${ind}.jpg`);
+        //  Replace region with white so we don't do things twice
+        gray.drawRectangle(cont.boundingRect(), new cv.Vec3(255, 255, 255), -1);
+    });
+    // cv.imshow('a window name', gray.resize(0, 0, 0.9, 0.9))
+    // cv.waitKey()
+    return names;
 }
 
 // getCrops("pic.jpg")
 
 module.exports = {
-  getCrops: getCrops
+    getCrops: getCrops
 };
--- latexer.js
+++ latexer.js
@@ -1,95 +1,82 @@
-const crop = require('./crop')
-const axios = require('axios')
-const fs = require('fs')
-const latex = require('node-latex')
-const style = require('./defaultStyle')
-const {spawn} = require('child_process')
-const Readable = require('stream').Readable
+const crop = require("./crop");
+const axios = require("axios");
+const fs = require("fs");
+const latex = require("node-latex");
+const style = require("./defaultStyle");
+const { spawn } = require("child_process");
+const Readable = require("stream").Readable;
 
-require('dotenv').config()
+require("dotenv").config();
 
-async function processImage(filename, appid, appkey) {
-  console.log(`PROCESSING ${filename}`)
-  let croppedFiles = crop.getCrops(filename)
-  console.log(`FINISHED PROCESSING ${filename}`)
-  let responses = []
-  console.log(croppedFiles)
-  const jobs = croppedFiles.map(async (croppedName) => {
-    // console.log('name', croppedName)
-    const b64 = fs.readFileSync(`./crops/${croppedName}`, 'base64')
-    // console.log(b64)
-    let imageURI = `data:image/jpg;base64,${b64}`
-    let config = {
-      headers: {
-        'app_id': appid,
-        'app_key': appkey,
-        'Content-type': 'application/json',
-      },
-    }
-    try {
-      let response = await
-          axios.post('https://api.mathpix.com/v3/latex', {
-            src: imageURI,
-            ocr: ['math', 'text'],
-          }, config)
-      if (response.data.latex) {
-        responses.push(response.data.latex)
-      }
-    } catch (err) {
-      console.log(err)
-    }
-  })
-  Promise.all(jobs).then(() => {
-    let outLatex = style.head
-    responses.forEach(resp => {
-      outLatex += style.prefix + resp.replace(/\\\\/g, '\\') + style.postfix
-    })
-    outLatex += style.tail
-    // console.log("________")
-    // console.log(outLatex)
-    // console.log("___________")
-    // const pdf = latex(outLatex)
-    // const output = fs.createWriteStream('./out.pdf')
-    // pdf.pipe(output).
-    //     on('error', err => console.error(err)).
-    //     on('finish', () => console.log('PDF Generated!'))
-    console.log( process.env.PATH );
-    const ls = spawn('ls')
-
-    // ls.stdout.on('data', (data) => {
-    //   console.log(`stdout: ${data}`);
-    // });
-    //
-    // ls.stderr.on('data', (data) => {
-    //   console.log(`stderr: ${data}`);
-    // });
-    //
-    // ls.on('close', (code) => {
-    //   console.log(`child process exited with code ${code}`);
-    // });
-    const child = spawn(`pdflatex`, ['-output-directory', 'pdfs', `-jobname=${filename}`]).on('error', function(err) {
-      console.log(err)
-    })
-    let stringStream = new Readable
-    stringStream.push(outLatex)
-    stringStream.push("\n")
-    stringStream.push(null)
-
-    child.stdout.on('data', (data) => {
-      console.log(`child stdout: ${data}`)
-    })
-    child.stderr.on('err', (err) => {
-      console.error(`child stderr:\n${err}`);
+async function processImage(filename) {
+    console.log(`PROCESSING ${filename}`);
+    let croppedFiles = crop.getCrops(filename);
+    console.log(`FINISHED PROCESSING ${filename}`);
+    let responses = [];
+    const jobs = croppedFiles.map(async croppedName => {
+        const b64 = fs.readFileSync(`./crops/${croppedName}`, "base64");
+        let imageURI = `data:image/jpg;base64,${b64}`;
+        let config = {
+            headers: {
+                app_id: process.env.MATHPIX_APP_ID,
+                app_key: process.env.MATHPIX_APP_KEY,
+                "Content-type": "application/json"
+            }
+        };
+        try {
+            let response = await axios.post(
+                "https://api.mathpix.com/v3/latex",
+                {
+                    src: imageURI,
+                    ocr: ["math", "text"]
+                },
+                config
+            );
+            if (response.data.latex) {
+                responses.push(response.data.latex);
+            }
+        } catch (err) {
+            console.log(err);
+        }
     });
-    child.on('close', (code) => {
-      console.log(`child process exited with code ${code}`);
+    Promise.all(jobs).then(() => {
+        let outLatex = style.head;
+        responses.forEach(resp => {
+            outLatex +=
+                style.prefix + resp.replace(/\\\\/g, "\\") + style.postfix;
+        });
+        outLatex += style.tail;
+        const child = spawn(`pdflatex`, [
+            "-output-directory",
+            "pdfs",
+            `-jobname=${filename}`
+        ]).on("error", function(err) {
+            console.log(err);
+        });
+        let stringStream = new Readable();
+        stringStream.push(outLatex);
+        stringStream.push("\n");
+        stringStream.push(null);
+
+        // child.stdout.on("data", data => {
+        //     console.log(`child stdout: ${data}`);
+        // });
+        child.stderr.on("err", err => {
+            console.error(`child stderr:\n${err}`);
+        });
+        child.on("close", code => {
+            if (code == 0) {
+                console.log("SUCCESS");
+            } else {
+                console.log(`ERR: child process exited with code ${code}`);
+            }
+        });
+        stringStream.pipe(child.stdin).on("err", data => {
+            console.error(`pipe stderr:\n${data}`);
+        });
     });
-    stringStream.pipe(child.stdin).stderr.on('err', (data) => {
-      console.error(`pipe stderr:\n${data}`);
-    })
-  })
 }
 
 module.exports = {
-  processImage: processImage,
-}
+    processImage: processImage
+};
